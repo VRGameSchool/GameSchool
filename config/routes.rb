dd8fc98e67d712156f09c92e62051b3cf6e611a7
James Calhoun <jmacalhoun@gmail.com>
2016-04-06 19:40:25 -0700

    update nav links and add about page with custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/welcome/about', to: 'welcome#about', as: 'about'
   resources :courses
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
