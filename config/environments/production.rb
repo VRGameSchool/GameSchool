aa28af514457e76cb6fd311296b8277187c0e56c
James Calhoun <jmacalhoun@gmail.com>
2016-04-09 16:34:29 -0700

    last try to get these fucking images to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.serve_static_assets = true
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
