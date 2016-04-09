063bb715d98e1dc9667a39a04361ff85ac65307b
James Calhoun <jmacalhoun@gmail.com>
2016-04-09 15:28:46 -0700

    add google analytics to production site and use figaro gem for security

---
+++ db/migrate/20160409221555_add_location_to_course.rb
@@ -0,0 +1,7 @@
+class AddLocationToCourse < ActiveRecord::Migration
+  def change
+    add_column :courses, :longitude, :float
+    add_column :courses, :latitude, :float
+    add_column :courses, :address, :string
+  end
+end
