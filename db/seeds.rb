e3241e34c0df2351e6637f5f53505bf20c7f0ae6
James Calhoun <jmacalhoun@gmail.com>
2016-04-09 17:37:01 -0700

    update seed file to defautl jason as instructor

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,23 @@ entity = School.create(
   {
     name: "GameSchool",
     value_prop: "Bring Your own VR Exprience to Life",
-    about_us: "Game School was founded on the idea that anyone can learn to make Virtual Reality. We're game designers and self-taught coders, and we want to make the next generation of influencers in the world of Virtual Reality. There are tons of places that VR can go - from virtual tourism, to therapy, to training - and we want to help anyone who has an idea, to understand how to make it a (virtual) reality.",
+    about_us: "Game School was founded on the idea that anyone can learn to make
+    Virtual Reality. We're game designers and self-taught coders, and we want to
+    make the next generation of influencers in the world of Virtual Reality.
+    There are tons of places that VR can go - from virtual tourism, to therapy,
+    to training - and we want to help anyone who has an idea, to understand how
+    to make it a (virtual) reality.",
+  }
+)
+
+instructor = Instructor.creaet(
+  {
+    first_name: "Jason",
+    last_name: "Mathias",
+    title: "Unity & VR Instructor",
+    bio: "Jason studied game design at USC's Interactive Media and Games program,
+    the number one game design program in the USA. A self-taught coder, he knows
+    how to tackle the challenges that first-time coders face. He has worked on
+    everything from educational games about Chemistry to VR Boxing to mobile games."
   }
 )
