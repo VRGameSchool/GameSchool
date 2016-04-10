9f8415de8c04c37f25d27120967a5226e4a9ccfc
James Calhoun <jmacalhoun@gmail.com>
2016-04-09 17:06:03 -0700

    add devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+  end
   resources :schools
   resources :instructors
   root 'welcome#index'
