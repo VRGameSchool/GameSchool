3e08927ee8513ed27da09bf8d1075f6272364e55
James Calhoun <jmacalhoun@gmail.com>
2016-04-11 20:05:36 -0700

    for loop to show available courses on welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
 
   def index
+    @courses = Course.where(course_status: "Availability")
   end
 
   def dashboard
